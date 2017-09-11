137d0114c68d0101a277785d0a993a394618f714
youyo <1003ni2@gmail.com>
2017-09-11 16:13:16 +0900

    create zabbix-web

---
+++ web/zabbix.conf.php
@@ -0,0 +1,14 @@
+<?php
+global $DB;
+$DB['TYPE'] = 'MYSQL';
+$DB['SERVER'] = 'mysql';
+$DB['PORT'] = '0';
+$DB['DATABASE'] = 'zabbix';
+$DB['USER'] = 'root';
+$DB['PASSWORD'] = '';
+$DB['SCHEMA'] = '';
+$ZBX_SERVER = 'zabbix-server';
+$ZBX_SERVER_PORT = '10051';
+$ZBX_SERVER_NAME = 'zabbix-monitoring-system';
+$IMAGE_FORMAT_DEFAULT = IMAGE_FORMAT_PNG;
+?>
